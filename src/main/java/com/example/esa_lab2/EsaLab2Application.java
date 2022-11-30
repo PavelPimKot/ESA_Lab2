d298d4dfc6a44f1a2bce8cafee30de2586941131
pimuk <pimukov.pavel@gmail.com>
2022-11-30 22:37:21 +0400

    Initial commit

---
+++ src/main/java/com/example/esa_lab2/EsaLab2Application.java
@@ -0,0 +1,14 @@
+package com.example.esa_lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+public class EsaLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsaLab2Application.class, args);
+    }
+
+}
